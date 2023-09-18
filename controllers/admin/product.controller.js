577da5699caf23e052aefbaca091c3f0c43500ad
KhangVinh <vinhkhang378@gmail.com>
2023-09-18 23:30:08 +0700

    pagination

--- controllers/admin/product.controller.js
+++ controllers/admin/product.controller.js
@@ -1,14 +1,13 @@
 // [GET] /admin/products
-const Product = require("../../models/product.model")
+const Product = require("../../models/product.model");
 const filterStatusHelper = require("../../helpers/filterStatus");
 const searchHelper = require("../../helpers/search");
+const paginationHelper = require("../../helpers/pagination");
 
 module.exports.index = async (req, res) => {
 
   const filterStatus = filterStatusHelper(req.query);
   let objectSearch = searchHelper(req.query);
-
-  console.log(filterStatus);
 
   let find = {
     deleted: false
@@ -20,15 +19,24 @@ module.exports.index = async (req, res) => {
   if (req.query.keyword) {
     find.title = objectSearch.regex;
   }
+  //Pagination
+  let initPagination = {
+    currentPage: 1,
+    limitItems: 4
+  } 
+  const countProducts = await Product.count(find);
+  const objectPagination = paginationHelper(initPagination, req.query, countProducts);
 
-  const products = await Product.find(find);
+  const products = await Product.find(find)
+    .limit(objectPagination.limitItems)
+    .skip(objectPagination.skip);
 
-  console.log(req.query);
 
   res.render("admin/pages/products/index", {
     pageTitle: "Product List",
     products: products,
     filterStatus: filterStatus,
-    keyword: objectSearch.keyword 
+    keyword: objectSearch.keyword,
+    pagination: objectPagination
   });
 }
