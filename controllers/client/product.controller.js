e0b85592eae04d6a721d0be16c2828418e4f7cb9
KhangVinh <vinhkhang378@gmail.com>
2023-12-27 15:53:56 +0700

    Chi tiết sản phẩm client

--- controllers/client/product.controller.js
+++ controllers/client/product.controller.js
@@ -1,6 +1,6 @@
-// [GET] /products
 const Product = require("../../models/product.model")
 
+// [GET] /products
 module.exports.index = async (req, res) => {
   const products = await Product.find({
     status: "active",
@@ -16,4 +16,25 @@ module.exports.index = async (req, res) => {
     pageTitle: "Danh sách sản phẩm",
     products: newProducts
   });
+}
+
+// [GET] /products/detail/:slug
+module.exports.detail = async (req, res) => {
+  try {
+    const slug = req.params.slug;
+
+    const product = await Product.findOne({
+      slug: slug,
+      deleted: false,
+      status: "active"
+    })
+
+
+    res.render("client/pages/products/detail", {
+      pageTitle: "Chi tiết sản phẩm",
+      product: product
+    });
+  } catch (error) {
+    res.redirect("/");
+  }
 }
