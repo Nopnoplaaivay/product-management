71aade7ad6bb7f1ecaf0a56f961138412d2ea678
KhangVinh <vinhkhang378@gmail.com>
2023-12-27 10:41:48 +0700

    validate

--- public/admin/js/script.js
+++ public/admin/js/script.js
@@ -187,3 +187,19 @@ if (showAlert) {
     showAlert.classList.add("alert-hidden");
   })
 }
+
+// Upload image 
+const uploadImage = document.querySelector("[upload-image]");
+if (uploadImage) {
+  const uploadImageInput = uploadImage.querySelector("[upload-image-input]");
+  const uploadImagePreview = uploadImage.querySelector("[upload-image-preview]");
+
+  uploadImageInput.addEventListener("change", (e) => {
+    if (e.target.files.length) {
+      const image = URL.createObjectURL(e.target.files[0]);
+
+      uploadImagePreview.src = image;
+    }
+  });
+}
+// End Upload image 
---
+++ validates/admin/product.validate.js
@@ -0,0 +1,15 @@
+module.exports.createPost = (req, res, next) => {
+  if (!req.body.title) {
+      req.flash("error", `Tiêu đề không được để trống!`);
+      res.redirect('back');
+      return;
+  }
+
+  if (req.body.title.length < 5) {
+      req.flash("error", `Tiêu đề dài tối thiểu 5 kí tự!`);
+      res.redirect('back');
+      return;
+  }
+  console.log("Chạy qua đây");
+  next();
+}
