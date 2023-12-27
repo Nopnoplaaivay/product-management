9b6c18d92a9d73ce634dd1ad1fb0d417d59276d1
KhangVinh <vinhkhang378@gmail.com>
2023-12-27 19:48:20 +0700

    upload Cloud

--- controllers/admin/product.controller.js
+++ controllers/admin/product.controller.js
@@ -155,9 +155,9 @@ module.exports.createPost = async (req, res) => {
   req.body.position = req.body.position
     ? parseInt(req.body.position)
     : countProduct + 1;
-  if (req.file && req.file.filename) {
-    req.body.thumbnail = `/uploads/${req.file.filename}`;
-  }
+  // if (req.file && req.file.filename) {
+  //   req.body.thumbnail = `/uploads/${req.file.filename}`;
+  // }
   console.log(req.file)
   const product = new Product(req.body);
   await product.save();
--- routes/admin/product.route.js
+++ routes/admin/product.route.js
@@ -1,11 +1,13 @@
 const express = require("express");
 const router = express.Router();
 const multer = require("multer");
-const storageMulterHelper = require('../../helpers/storageMulter.js');
-const storage = storageMulterHelper();
-const upload = multer({ storage: storage });
+const uploadCloud = require("../../middlewares/admin/uploadCloud.middleware")
+// const storageMulterHelper = require('../../helpers/storageMulter.js');
+// const storage = storageMulterHelper();
+const upload = multer();
 const controller = require("../../controllers/admin/product.controller");
 const validate = require("../../validates/admin/product.validate");
+
 
 router.get("/", controller.index);
 
@@ -18,21 +20,23 @@ router.delete("/delete/:id", controller.deleteItem);
 router.get("/create", controller.create);
 
 router.post(
-  "/create", 
+  "/create",
   upload.single("thumbnail"),
-  validate.createPost, 
+  validate.createPost,
+  uploadCloud.upload,
   controller.createPost
 );
 
-router.get('/edit/:id', controller.edit)
+router.get("/edit/:id", controller.edit);
 
 router.patch(
-  "/edit/:id", 
+  "/edit/:id",
   upload.single("thumbnail"),
-  validate.createPost, 
+  uploadCloud.upload,
+  validate.createPost,
   controller.editPatch
 );
 
-router.get('/detail/:id', controller.detail)
+router.get("/detail/:id", controller.detail);
 
 module.exports = router;
