c38e90c60e30bcf227abddeee7650abae832ffe6
KhangVinh <vinhkhang378@gmail.com>
2023-12-27 23:14:14 +0700

    tiny mce

--- index.js
+++ index.js
@@ -1,23 +1,27 @@
-const express = require('express');
-const methodOverride = require('method-override');
+const express = require("express");
+const methodOverride = require("method-override");
 const bodyParser = require("body-parser");
 const flash = require("express-flash");
-const cookieParser = require("cookie-parser")
-const session = require("express-session")
+const cookieParser = require("cookie-parser");
+const session = require("express-session");
+const path = require('path');
 
 require("dotenv").config();
 
-const database = require('./config/database');
-const systemConfig = require('./config/system');
+const database = require("./config/database");
+const systemConfig = require("./config/system");
 
 database.connect();
 
 const routeClient = require("./routes/client/index.route.js");
 const routeAdmin = require("./routes/admin/index.route.js");
 
-
 const app = express();
 const port = process.env.PORT;
+
+// TinyMCE
+app.use('/tinymce', express.static(path.join(__dirname, 'node_modules', 'tinymce')));
+// End TinyMCE
 
 // Bien toan cuc
 app.locals.prefixAdmin = systemConfig.prefixAdmin;
@@ -26,15 +30,15 @@ app.locals.prefixAdmin = systemConfig.prefixAdmin;
 app.set("views", `${__dirname}/views`);
 app.set("view engine", "pug");
 
-// Flash 
-app.use(cookieParser('...'));
-app.use(session({ cookie: { maxAge: 60000 }}));
+// Flash
+app.use(cookieParser("..."));
+app.use(session({ cookie: { maxAge: 60000 } }));
 app.use(flash());
-// End Flash 
+// End Flash
 
 app.use(express.static(`${__dirname}/public`));
 
-app.use(methodOverride('_method'));
+app.use(methodOverride("_method"));
 // parse application/x-www-form-urlencoded - Convert data của form để truyền cho biến req.body
 app.use(bodyParser.urlencoded({ extended: false }));
 
@@ -44,5 +48,5 @@ routeAdmin(app);
 // End Routes
 
 app.listen(port, () => {
-    console.log(`App listening on http://127.0.0.1:${port}`);
+  console.log(`App listening on http://127.0.0.1:${port}`);
 });
