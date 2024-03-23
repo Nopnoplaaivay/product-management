78ed6e34b243de2e9b4fc654bb993073217615d4
KhangVinh <vinhkhang378@gmail.com>
2024-03-23 14:27:12 +0700

    Xay dung danh sach tai khoan

--- routes/admin/index.route.js
+++ routes/admin/index.route.js
@@ -3,6 +3,7 @@ const productRoutes = require("./product.route");
 const productsCategoryRoutes = require("./products-category.route");
 const roleRoutes = require("./role.route");
 const accountRoutes = require("./account.route");
+const authRoutes = require("./auth.route");
 
 const systemConfig = require('../../config/system')
 
@@ -14,4 +15,5 @@ module.exports = (app) => {
   app.use(PATH_ADMIN + "/products-category", productsCategoryRoutes);
   app.use(PATH_ADMIN + "/roles", roleRoutes);
   app.use(PATH_ADMIN + "/accounts", accountRoutes);
+  app.use(PATH_ADMIN + "/auth", authRoutes);
 }
