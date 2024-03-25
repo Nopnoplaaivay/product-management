6c834b34b5426e5c4943d719977efb1e7f22a603
KhangVinh <vinhkhang378@gmail.com>
2024-03-25 20:23:15 +0700

    hien thi danh sach noi bat

--- controllers/client/home.controller.js
+++ controllers/client/home.controller.js
@@ -1,6 +1,20 @@
+const Product = require("../../models/product.model");
+const productsHelper = require("../../helpers/products");
+
 // [GET] /
 module.exports.index = async (req, res) => {
+  // Hiển thị danh sách sản phẩm
+  const productsFeatured = await Product.find({
+    featured: "1",
+    deleted: false,
+    status: "active",
+  }).limit(6);
+  
+  const newProductsFeatured = productsHelper.priceNewProducts(productsFeatured);
+  console.log(productsFeatured);
+
   res.render("client/pages/home/index", {
-    pageTitle: "Trang chủ"
+    pageTitle: "Trang chủ",
+    productsFeatured: newProductsFeatured
   });
-}
+};
