7ffbd7632c2a0bb81166563e1c573e30d76e9ecb
KhangVinh <vinhkhang378@gmail.com>
2024-03-25 17:34:07 +0700

    update user info

--- routes/admin/index.route.js
+++ routes/admin/index.route.js
@@ -4,6 +4,7 @@ const productsCategoryRoutes = require("./products-category.route");
 const roleRoutes = require("./role.route");
 const accountRoutes = require("./account.route");
 const authRoutes = require("./auth.route");
+const myAccountRoutes = require("./my-account.route");
 
 const authMiddleware = require("../../middlewares/admin/auth.middleware");
 
@@ -25,5 +26,6 @@ module.exports = (app) => {
   );
   app.use(PATH_ADMIN + "/roles", authMiddleware.requireAuth, roleRoutes);
   app.use(PATH_ADMIN + "/accounts", authMiddleware.requireAuth, accountRoutes);
+  app.use(PATH_ADMIN + "/my-account", authMiddleware.requireAuth, myAccountRoutes);
   app.use(PATH_ADMIN + "/auth", authRoutes);
 };
