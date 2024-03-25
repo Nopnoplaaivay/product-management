f8d4f516636e2bed6faefe1b35f42bcfee91892b
KhangVinh <vinhkhang378@gmail.com>
2024-03-25 19:42:14 +0700

    featured noi bat

--- middlewares/client/category.middleware.js
+++ middlewares/client/category.middleware.js
@@ -1,14 +1,31 @@
-const ProductCategory = require("../../models/products-category.model");
-const createTree = require("../../helpers/createTree.js");
+const ProductCategory = require('../../models/products-category.model.js');
+// const PostCategory = require('../../models/posts-category.model.js')
 
-module.exports.category = async (res, req, next) => {
+const createTree = require('../../helpers/createTree.js')
+
+
+module.exports.category = async (req, res, next) => {
+  //! category product
   const categoryProducts = await ProductCategory.find({
-    deleted: false
-  });
+    deleted: false,
+    status: "active"
+  })
 
-  const newCategoryProducts = createTree(categoryProducts);
+  const newCategoryProducts = createTree(categoryProducts)
 
-  res.locals.layoutCategoryProducts = newCategoryProducts;
+  res.locals.layoutCategoryProducts = newCategoryProducts
+  //! end category product
 
-  next(); 
+  // //! category post
+  // const categoryPosts = await PostCategory.find({
+  //   deleted: false,
+  //   status: "active"
+  // })
+
+  // const newCategoryPosts = createTree(categoryPosts)
+
+  // res.locals.layoutCategoryPosts = newCategoryPosts
+  // //! end category post
+
+  next();
 }
--- routes/client/index.route.js
+++ routes/client/index.route.js
@@ -1,6 +1,6 @@
+const categoryMiddleware = require("../../middlewares/client/category.middleware");
 const homeRoutes = require("./home.route");
 const productRoutes = require("./product.route");
-const categoryMiddleware = require("../../middlewares/client/category.middleware");
 
 module.exports = (app) => {
   // Tạo category để tất cả các route đều dùng được
